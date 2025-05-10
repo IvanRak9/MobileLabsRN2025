64f845a99f5db73a5e225e43c2d20756bd84994e
ipz232_ris <ipz232_ris@student.ztu.edu.ua>
2025-05-10 14:12:16 +0300

    commit Lab1

--- App.js
+++ App.js
@@ -1,20 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { View, Text, Image, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 
-export default function App() {
+import HomeScreen from './screens/HomeScreen';
+import GalleryScreen from './screens/GalleryScreen';
+import ProfileScreen from './screens/ProfileScreen';
+
+const Tab = createBottomTabNavigator();
+
+function CustomHeader() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.headerContainer}>
+      <Image
+        source={{
+          uri: 'https://pdp.nacs.gov.ua/system/providers/logos/000/000/057/original/%D0%B7%D0%B0%D0%B2%D0%B0%D0%BD%D1%82%D0%B0%D0%B6%D0%B5%D0%BD%D0%BD%D1%8F_%282%29.png?1651503202',
+        }}
+        style={styles.logo}
+      />
+      <Text style={styles.headerText}>FirstMobileApp</Text>
     </View>
   );
 }
 
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          headerTitle: () => <CustomHeader />,
+          tabBarIcon: ({ color, size }) => {
+            let iconName;
+            if (route.name === 'Головна') iconName = 'home';
+            else if (route.name === 'Фотогалерея') iconName = 'images';
+            else if (route.name === 'Профіль') iconName = 'person';
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+      >
+        <Tab.Screen name="Головна" component={HomeScreen} />
+        <Tab.Screen name="Фотогалерея" component={GalleryScreen} />
+        <Tab.Screen name="Профіль" component={ProfileScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  headerContainer: {
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
+    gap: 10,
+  },
+  logo: {
+    width: 100,
+    height: 100,
+    resizeMode: 'contain',
+  },
+  headerText: {
+    fontSize: 18,
+    fontWeight: 'bold',
   },
 });
